ca56704c452d3ceb0d0e98a3e556672430d89074
n4n5 <its.just.n4n5@gmail.com>
2024-09-04 15:40:31 +0200

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    base: "/thoughts",
+    base: import.meta.env.DEV ? "." : "/thoughts",
 });
