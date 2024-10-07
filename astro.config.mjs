b2263dea2df799f23f29c05a8712d15bd3d4f90d
n4n5 <its.just.n4n5@gmail.com>
2024-10-07 14:08:35 +0200

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import rehypeExternalLinks from "rehype-external-links";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    site: "https://thoughts.n4n5.dev",
+    integrations: [
+        mdx({
+            rehypePlugins: [[rehypeExternalLinks, { target: "_blank" }]],
+        }),
+        sitemap({
+            changefreq: "weekly",
+            priority: 0.7,
+            lastmod: new Date(),
+        }),
+    ],
 });
