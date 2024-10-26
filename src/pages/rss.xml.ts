3174a071b7dff0952316bb1f2c67bd9800c68b08
n4n5 <its.just.n4n5@gmail.com>
2024-10-26 17:05:38 +0200

    sort

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,14 +1,15 @@
 import type { APIContext } from "astro";
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import { getAllPosts, sortMDByDate } from "../utils/posts";
 
 export async function GET(context: APIContext) {
-    const blog = await getCollection("post");
+    const allPosts = await getAllPosts();
+    const allPostsByDate = sortMDByDate(allPosts);
     return rss({
         title: "Thoughts of n4n5",
         description: "RSS feed for thoughts by n4n5",
         site: context.site || new URL("https://thoughts.n4n5.dev"),
-        items: blog.map((post) => ({
+        items: allPostsByDate.map((post) => ({
             title: post.data.title,
             pubDate: post.data.publishDate,
             link: `/${post.data.customSlug ?? post.slug}/`,
