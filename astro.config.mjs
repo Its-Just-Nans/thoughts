94a6071ab083e0f51de4442ed65edaef78c9379c
n4n5 <its.just.n4n5@gmail.com>
2025-10-21 18:36:46 -0600

    download in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
 import { thisWebsite } from "./src/utils/constants";
+import { downloadScript } from "./src/utils/utils";
 
 import mdx from "@astrojs/mdx";
+
+await downloadScript({
+    "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css": "leaflet.css",
+    "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js": "leaflet.js",
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png": "images/marker-icon.png",
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png": "images/marker-shadow.png",
+});
 
 // https://astro.build/config
 export default defineConfig({
